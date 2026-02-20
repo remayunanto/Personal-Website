204a6f6780b103e626ff510db2249e8c08a0e05e
Rema Prostinaldo Yunanto <65830489+remayunanto@users.noreply.github.com>
2026-02-20 23:58:23 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Change AUM here
-const aum = 32870267; // IDR value
-
-function formatIDR(number) {
-    return "IDR " + number.toLocaleString("id-ID");
-}
-
-document.getElementById("aumValue").innerText = formatIDR(aum);
